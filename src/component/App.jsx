3fcd20c0e30d042f778ad696b73ed6d725b5d650
unknown <amarkumar9651@gmail.com>
2022-01-01 12:41:33 +0530

    initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../Notes.js";
+
+
+
+function App(){
+    return (
+        <div>
+            <Header />
+            {notes.map(note => 
+                (<Note
+                    key={note.id}
+                    name={note.title}
+                    description={note.content}
+                    />
+                )
+            )}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
